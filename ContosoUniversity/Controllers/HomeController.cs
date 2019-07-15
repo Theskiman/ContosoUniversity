7023aed3a1431c1fe54a321ba328ca9f5fc57a46
unknown <cliftonmatuszewski@gmail.com>
2019-07-15 11:27:09 -0500

    Added about page

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -5,12 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity.Models;
-
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        
+            private readonly SchoolContext _context;
+
+            public HomeController(SchoolContext context)
+            {
+                _context = context;
+            }
+            public IActionResult Index()
         {
             return View();
         }
@@ -18,6 +27,18 @@ namespace ContosoUniversity.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
